2cd1ef57342fa1e651db519f07ada05fcec0c2c2
Craig Smarkus <infected.shadow0@gmail.com>
2016-01-12 13:50:57 -0500

    Refactored to use discord.py async
    Rewrote the core of the bot to use the async version of discord.py.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # CraigBot
 A bot for my discord server
 
-# Setup
-For the bot to work a settings.txt file must be created with the contents being the login credentials for the bot in the format <emailaddress>:<password>
+## Setup
+Enter your account credentials into the settings.json section for the login and run the bot.py script
+
+## Requirements
+- Python 3.4.2+
+- Discord.py async branch
--- bot.py
+++ bot.py
@@ -1,143 +1,72 @@
 import discord
-import time
+import asyncio
 import random
 import json
-import threading
 
-f = open("settings.txt", "r")
-loginInfo = f.read().split(":")
-f.close()
+with open('settings.json') as f:
+	settings = json.load(f)
 
-admins = []
-status = { 
-	"debug": False,
-	"trivia": False,
-	"trivia_channel": ""
-}
+with open('insults.json') as f:
+	insults = json.load(f)
 
 client = discord.Client()
-client.login(loginInfo[0], loginInfo[1])
 
-if not client.is_logged_in:
-	print("SYS: Failed to login")
-	exit(1)
+async def HandleCommand(msg, client):
+	args = msg.content.split(' ')
+	command = args[0].strip(settings['commandCharacter']).lower()
+	args.pop(0)
 
-def handleCommand(args, msg):
-	global status
-	global admins
+	if command == 'help':
+		await client.send_message(msg.channel, 'I am a bot. I insult Hex.')
+		await client.send_message(msg.channel, '__**Commands**__')
+		await client.send_message(msg.channel, '**!help** - list of commands')
+		await client.send_message(msg.channel, '**!exit** *admin only* - close the bot')
+		await client.send_message(msg.channel, '**!admins** - List of admins [Work in Progress]')
+		await client.send_message(msg.channel, '**!insult** - Insults the first mentioned user')
+		await client.send_message(msg.channel, '**!addinsult** - Add an insult to the bots insult list. Denote the insultee with $user (tts currently disabled)')
+	elif command == 'insult':
+		await client.send_message(msg.channel, (random.choice(insults)).replace('$user', args[1]))
+	elif command == 'addinsult':
+		insults.append(' '.join(args))
+		saveFile('insults.json', insults)
+		await client.send_message(msg.channel, 'Added')
+	elif command == 'admins':
+		await client.send_message(msg.channel, 'Bot Admins:')
+		for admin in settings['admins']:
+			await client.send_message(msg.channel, admin)
 
-	command = args[0].strip("!").lower()
-
-	if command == "help":
-		print("SYS: Replying with help")
-		client.send_message(msg.channel, "I am a bot. I insult Hex.")
-		client.send_message(msg.channel, "__**Commands**__")
-		client.send_message(msg.channel, "**!help** - List of commands")
-		client.send_message(msg.channel, "**!exit** *admin only* - close the bot")
-		client.send_message(msg.channel, "**!admins** - List of bot admins [WIP]")
-		client.send_message(msg.channel, "**!setadmin** *admin only* - set mentioned users as admins on the bot")
-		client.send_message(msg.channel, "**!removeadmin** *admin only* - removes mentioned users from being bot admins")
-		client.send_message(msg.channel, "**!addinsult** - adds an insult to the list of available insults")
-		client.send_message(msg.channel, "**!insult** - insults the first mentioned user")
-	elif command == "exit":
+		await client.send_message(msg.channel, 'Server Admins:')
+		for member in msg.server.members:
+			for role in member.roles:
+				if role.name == settings['adminRole']:
+					await client.send_message(msg.channel, member.name)
+	elif command == 'flip':
+		await client.send_message(msg.channel, random.choice(["HEADS", "TAILS"]))
+	elif command == 'exit':
 		if checkPrivilege(msg.author.id):
-			print("SYS: Closing...")
 			exit(1)
-	elif command == "setadmin":
-		if checkPrivilege(msg.author.id):
-			for user in msg.mentions:
-				if user.id not in admins:
-					print("SYS: Adding {} as admin".format(user.name))
-					admins.append(user.id)
 
-			f = open("admins.txt", "w")
-			f.write(",".join(admins))
-			f.close()
-	elif command == "removeadmin":
-		if checkPrivilege(msg.author.id):
-			for user in msg.mentions:
-				print("SYS: Removing {} as admin".format(user.name))
-				admins.remove(user.id)
-
-			f = open("admins.txt", "w")
-			f.write(",".join(admins))
-			f.close()
-	elif command == "admins":
-		print("SYS: Listing admins")
-		client.send_message(msg.channel, "Current bot admins are:")
-		for admin in admins:
-			client.send_message(msg.channel, "- {}".format(admin))
-	elif command == "insult":
-		insults = open("insults.txt", "r").read().splitlines()
-		insult = random.choice(insults)
-		tts = False
-
-		if len(args) > 2:
-			if args[2] == "tts":
-				tts = True
-
-		client.send_message(msg.channel, insult.replace("$user", args[1]), True, tts)
-	elif command == "addinsult":
-		insult = args
-		insult.pop(0)
-		f = open("insults.txt", "a")
-		f.write(" ".join(insult) + "\n")
-		f.close()
-		client.send_message(msg.channel, "Added")
-	elif command == "trivia":
-		if args[1] == "on":
-			status["trivia"] = True
-			status["trivia_channel"] = msg.channel.id
-	elif command == "status":
-		if checkPrivilege(msg.author.id):
-			client.send_message(msg.channel, "```" + json.dumps(status) + "```")
-	elif command == "flip":
-		client.send_message(msg.channel, random.choice(["HEADS", "TAILS"]))
-	elif command =="quote":
-		tts = False
-
-		if len(args) > 2:
-			if args[2] == "tts":
-				tts = True
-
-		client.send_message(msg.channel, getQuote(args[1]), True, tts)
-
-def getQuote(name):
-	with open("quotes.txt") as f: 
-		quotes = open("quotes.txt", "r").read().splitlines()
-		quote = random.choice(quotes)
-		if name == quote.partition("|")[0]:
-			return quote.partition("|")[1]
-		
+def saveFile(file, data):
+	with open(file, 'w') as f:
+		json.dump(data, f, indent = 4, sort_keys = True)
 
 def checkPrivilege(id):
-	if id in admins:
+	if id in settings['admins']:
 		return True
 	else:
 		return False
 
-@client.event
-def on_message(msg):
-	if msg.author.id == client.connection.user.id:
-		return
+@client.async_event
+async def on_message(msg):
+	if msg.content.startswith(settings['commandCharacter']):
+		await HandleCommand(msg, client)
 
-	print("MSG: [{}:{}] -> {}:{} : {}".format(str(msg.author), msg.author.id, msg.channel.name, msg.channel.id, msg.content.encode("utf-8")))
+@client.async_event
+async def on_ready():
+	print('SYS: Bot is running as {}'.format(client.user.name))
 
-	if msg.content.startswith("!"):
-		args = msg.content.split(" ")
-		handleCommand(args, msg)
+def main():
+	client.run(settings['login']['email'], settings['login']['password'])
 
-@client.event
-def on_ready():
-	global admins
-
-	print("SYS: Loading admins...")
-
-	f = open("admins.txt", "r")
-	admins = f.read().split(",")
-	f.close()
-
-	print("SYS: Bot is running as {}".format(client.user.name))
-
-print("SYS: Starting bot...")
-client.run()
+if __name__ == '__main__':
+	main()
